000f271b0191871819eb3bd758d5dc9d17dd2f02
Amithkumar9292 <amithnaik429@gmail.com>
2024-12-07 17:16:56 +0530

    Added first code

---
+++ jSession.java
@@ -0,0 +1,20 @@
+package hh;
+
+public class jSession {
+
+	public static void main(String[] args) {
+		int i=0;
+		int j=0;
+		while(i<3) {
+			j=0;
+			while(j<3) {
+				System.out.print("JAVA MASTERS ");
+				j++;
+			}
+			i++;
+			System.out.println();
+		}
+
+	}
+
+}
